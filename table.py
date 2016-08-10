3726163303058843784cdd0b84c2da97ce1ed743
marshki <marshall@morethantomorrow.org>
2016-08-09 21:19:07 -0400

    More tabulate testing

--- table.py
+++ table.py
@@ -1,4 +1,5 @@
 #!/bin/bash
 # table fomratting with tablulate 
 from tabulate import tabulate 
-print (tabulate([['Ben Dover', 99, 'Flushing'], ['Seymour Butts', 21, 'Bayside']], headers=['Name', 'Age', 'Neighborhood']))
+print tabulate([['Ben Dover', 99, 'Flushing'], ['Seymour Butts', 21, 'Bayside']], headers=['Name', 'Age', 'Neighborhood'])
+print tabulate([['1', '2, 3, 4, 5, 6', '7'], ['2', '8, 9, 10, 11, 12', '13']], headers=['Run #', 'White #s', 'Red #'], tablefmt='orgtbl')
