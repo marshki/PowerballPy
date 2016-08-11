519a1dccb644877b5d65619c2a9d1a80f585688e
marshki <marshall@morethantomorrow.org>
2016-08-11 16:05:21 -0400

    update table formatting

--- table.py
+++ table.py
@@ -1,35 +1,42 @@
 #!/bin/bash
-# table fomratting with tablulate 
-from tabulate import tabulate 
-import random 
+# Python 2 or 3 
 
+"""Pseudo-random number generator for 'Powerball' lottery"""
+ 
+# Import Python modules 
+ 
+import random					# for number generation  
+from prettytable import PrettyTable 		# for tabular formatting 
 
-print tabulate([['Ben Dover', 99, 'Flushing'], ['Seymour Butts', 21, 'Bayside']], headers=['Name', 'Age', 'Neighborhood'])
+# Define functions 
 
-print
+def user_runs(): 
+	"""Prompt user for number of runs"""
+	return int(input('Enter number of runs:  '))
 
-print tabulate([['1', '2, 3, 4, 5, 6', '7'], ['2', '8, 9, 10, 11, 12', '13']], headers=['Run #', 'White #s', 'Red #'], tablefmt='orgtbl')
-
-print 
 
 def white_nums():
-        """Generate 5 'white ball' numbers between 1 and 69--inclusive, no duplicates"""
-        whites = random.sample(range(1,69),5)
-	return(', '.join(map(str, whites)))
+	"""Generate 5 'white ball' numbers between 1 and 69--inclusive, no duplicates"""
+	whites = random.sample(range(1,69),5)
+	return(', '.join(map(str, whites)))		# strip non-integer characters, separate numbers with commas 
 
 
 def red_num():
-        """Generate 1 'red ball' number between 1 and 26--inclusive"""
-        red = random.sample(range(1,26),1)
-        return(', '.join(map(str, red)))       
+	"""Generate 1 'red ball' number between 1 and 26--inclusive""" 
+	red = random.sample(range(1,26),1)	
+	return(', '.join(map(str, red)))		# strip non-integer characters, separate numbers with commas 
+	
 
-
-whitey = white_nums()
-redy = red_num()
-
-print tabulate([[whitey, redy]], headers=['White #s', 'Red #'], tablefmt='orgtbl')
-
-
-
-
- 
+def program():
+	"""Guts of the program"""
+	table = PrettyTable(['White #s', 'Red #'])	# create table w/headers 
+	runs = user_runs()				
+	for int in range(runs):
+		white = white_nums() 
+		red = red_num()
+		table.add_row([white, red])  		# add rows to table 
+	return table 
+		
+print("\nWelcome to the Powerball Number Generator!\n")
+pretty_table = program()
+print(pretty_table) 
