67db677e4a65c91a4a67a2cf092c8255739e4731
marshki <marshall@morethantomorrow.org>
2017-05-31 18:18:28 -0400

    add improved version

---
+++ pwrBall.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+from builtins import input 
+
+#!/usr/bin/env Python
+#!/usr/bin/env Python3 
+# Python 2 or 3 
+
+""" Pseudo-random number generator for 'Powerball' lottery """
+
+# Import Python modules 
+import random                                   # for number generation 
+from prettytable import PrettyTable             # for tabular formatting 
+
+# Create table for displaying number with headers; align output to left 
+
+table = PrettyTable(['White #s', 'Red #'])
+table.align = "l"
+
+# Define functions 
+
+def userInput():
+    """ Prompt user for Y/N input """ 
+    while True: 
+        userChoice = str(input('Generate Powerball numbers? Y/N: ')).lower()
+        if userChoice == 'y' or userChoice == 'n': 
+            return userChoice  
+
+def userRuns(): 
+    """ Prompt user for number of runs """
+    return int(input('Enter number of runs: '))
+
+def whiteNums(): 
+    """ Generate 5 'white ball' numbers between 1 and 69--inclusive, no duplicates"""
+    whites = random.sample(range(1, 69 + 1),5)  # Python range stops at y - 1 in range(x, y)
+    return(', '.join(map(str, whites)))         # strip non-integer characters, separate numbers with commas 
+
+def redNum(): 
+    """ Generate 1 'red ball' number between 1 and 26--inclusive """ 
+    red = random.sample(range(1, 26 + 1),1)     # Python range stops at y - 1 in range (x, y)
+    return(', '.join(map(str, red)))            # as above 
+
+def generateNums(): 
+    """ Create table with Powerball numbers  """ 
+    runs = userRuns()
+    for int in range(runs): 
+        white = whiteNums()
+        red = redNum()
+        table.add_row([white, red])
+    return table 
+
+def powerball(): 
+    """ Guts of the program  """ 
+    while True: 
+        runMe = userInput() 
+        if runMe == 'y': 
+            generate = generateNums()
+            print(generate)
+            continue 
+        else: 
+            print('Ciao!')
+            break 
+            
+# Let's play!
+
+if __name__ == '__main__': 
+    print("\nWelcome to the Powerball Number Generator!\n")
+    prettyTable = powerball()
+
