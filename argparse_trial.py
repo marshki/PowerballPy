b34398b30723d6c8b1ff83fd303bd37c3c1d767e
marshki <marshall@morethantomorrow.org>
2017-06-06 18:59:04 -0400

    add comments

--- argparse_trial.py
+++ argparse_trial.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python 
 
-import argparse 
+import argparse         # module for argument parsing 
+
+# Arguments 
 
 parser = argparse.ArgumentParser(description='Do magic with integers.')
 
+# integers attribute will be a list of one or more ints 
+
 parser.add_argument('integers', metavar='N', type=int, nargs='+',
                     help='an integer for the accumulator')
+
+# accumulate attribute will be either a sum or max function, per user call
 
 parser.add_argument('--sum', dest='accumulate', action='store_const',
                     const=sum, default=max, 
