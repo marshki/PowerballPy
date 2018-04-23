cc826cfdb5d82e16f0b7dc0cfd9ed50e4fb70352
marshki <marshall@morethantomorrow.org>
2018-04-23 10:36:09 -0400

    add test dir

---
+++ tests/test_powerball_2.0.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env Python3 
+""" 
+Unit tests.
+"""
+
+# Import functions 
+
+import unittest 
+from powerball_2 import * 
+
+# Define testing class 
+
+import random
+
+def white_nums(range_min, range_max, balls):
+    return set(random.sample(range(range_min, range_max + 1), balls))
+
+
+class TestWhiteNums(unittest.TestCase):
+
+    def setUp(self):
+        self.range_min = 1
+        self.range_max = 69
+        self.balls = 5
+        self.white_balls = white_nums(self.range_min, self.range_max, self.balls)
+
+    def test_count(self):
+        self.assertEqual(len(self.white_balls), self.balls)
+
+    def test_range(self):
+        for white_ball in self.white_balls:
+            self.assertGreaterEqual(white_ball, self.range_min)
+            self.assertLessEqual(white_ball, self.range_max)
+
+    def test_duplicates(self):
+       self.assertEqual(self.white_balls, set(self.white_balls))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+unittest.main()
+
