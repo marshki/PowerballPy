0d7624163c4bb2666419f7104db5e23d5d510d39
marshki <marshall@morethantomorrow.org>
2019-10-29 10:59:39 -0400

    add class for y/n input unit test

--- tests/test_powerball.py
+++ tests/test_powerball.py
@@ -7,5 +7,26 @@ from __future__ import print_function
 from builtins import input
 
 import unittest
-import random
-from prettytable import PrettyTable
+#import random
+#from prettytable import PrettyTable
+
+def yes_no_input():
+    """Prompt user for Y/N input.
+    """
+
+    while True:
+        try:
+            user_input = str(input('Generate Powerball numbers? Y/N: ')).lower()
+            if user_input in ['y', 'n']:
+                return user_input
+        except ValueError: 
+            print('Try again')
+
+class YesNoInput(unittest.TestCase):
+
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+
