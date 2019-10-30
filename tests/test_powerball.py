1252275f10b6ab397d58735c2a50db92feba4917
marshki <marshall@morethantomorrow.org>
2019-10-30 14:58:13 -0400

    just for testing

--- tests/test_powerball.py
+++ tests/test_powerball.py
@@ -7,8 +7,8 @@ from __future__ import print_function
 from builtins import input
 
 import unittest
-#import random
-#from prettytable import PrettyTable
+
+from unittest.mock import patch
 
 def yes_no_input():
     """Prompt user for Y/N input.
@@ -29,4 +29,11 @@ class YesNoInput(unittest.TestCase):
     For reference: https://docs.python.org/3/library/unittest.mock.html
     """
 
+    @patch('builtins.input', return_value='y')
+    def test_yes_no_input_01(self, input):
+        """Valid return value.
+        """
+        self.assertEqual(yes_no_input(), user_input)
 
+if __name__ == '__main__':
+    unittest.main()
