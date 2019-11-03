27fd1020124c7e2d213a32b8ab12829dc6482d86
marshki <marshall@morethantomorrow.org>
2019-11-03 09:01:48 -0500

    fix linter errors

--- tests/test_powerball.py
+++ tests/test_powerball.py
@@ -19,7 +19,7 @@ def yes_no_input():
             user_input = str(input('Generate Powerball numbers? Y/N: ')).lower()
             if user_input in ['y', 'n']:
                 return user_input
-        except ValueError: 
+        except ValueError:
             print('Try again')
 
 class YesNoInput(unittest.TestCase):
