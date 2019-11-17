14cf3471780bcb7f30fce783db77d876ad769790
marshki <marshall@morethantomorrow.org>
2019-11-16 20:08:20 -0500

    add

---
+++ tests/test_numerical_input.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""Unit test for powerball.py.
+"""
+
+import unittest
+
+from unitest.mock import patch
+
+def numerical_input():
+    """Prompt user for numerical input.
+    """
+
+    while True:
+        try:
+            return int(input('How many sets of Powerball numbers should we generate?: '))
+        except ValueError:
+            print('Try again')
+
+class NumericalInput(unittest.TestCase):
+
+    """Unit tests.
+    Use `patch()` to mock objects for testing.
+    For reference: https://docs.python.org/3/library/unittest.mock.html
+    """
+
+    @patch('builtins.input', return_value='10')
+    def test_numerical_input_01(self, input):
+        """Valid return value.
+        """
+        self.assertEqual(numerical_input(), 'y')
+
+    @patch('builtins.input', return_value='n')
+    def test_numerical_input_02(self, input):
+        """Valid return value.
+        """
+        self.assertEqual(numerical_input(), 'n')
+
+if __name__ == '__main__':
+    unittest.main()
