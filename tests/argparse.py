609afd1b33bf7fceed4225d6307f65ac481d069a
marshki <marshall@morethantomorrow.org>
2021-09-23 11:39:39 -0400

    add placeholder

---
+++ tests/argparse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python 3
+
+def parse_cli_args():
+    """Define command line parser w/arguments.
+    """
+    parser = argparse.ArgumentParser(description="PowerballPy.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--", help="define n of sets to generate", nargs='+', type=float)
+
+    args = parser.parse_args()
+
+    if args.n_sets is None:
+        parser.error("Argument required. None given.")
+
+    return args
