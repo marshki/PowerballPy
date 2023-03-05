e7ec83e60dc1fe1c1d5597de25b65944a42c4ce5
marshki <marshall@morethantomorrow.org>
2023-03-05 12:35:52 -0500

    add unit testing framework, but need to write valid tests

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -1,18 +1,49 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
 
-"""Define set(s) of numbers to generate using argument parsing."""
+"""Define sets of numbers to generate using argparse."""
 
 import argparse
 
+import unittest
+
 def parse_cli_args():
-    """Placeholder.
+    """Command line parser for user-defined sets of numbers.
+    Args:
+      --sets n
+    Returns:
+        int. e.g. n
     """
 
     parser = argparse.ArgumentParser(description="Set(s) of PowerBall numbers to generate.")
-    parser.add_argument("--sets", action="store", type=float,\
+    parser.add_argument("--sets", action="store", type=int,\
                         required=False,\
                         help="Number of sets to generate, e.g. 5")
 
-    args = parser.parse.args()
-    #print(args)
-    return args
+    return parser
+
+class ParseCLIArgs(unittest.TestCase):
+
+    """
+    Unit tests.
+    """
+    def setUp(self):
+        self.parser = parse_cli_args()
+
+    def test_parser_cli_args_01(self):
+        parsed = self.parser.parse_args(['--sets', '5'])
+        self.assertTrue(type(self.sets) is int)
+
+
+    #def test_parser_cli_args_01(self):
+    #    """Valid return value."""
+    #    parsed = self.parser.parse_args(['--sets', '5'])
+    #    self.assertEqual(parsed.sets, 5)
+
+    #def test_parser_cli_args_02(self):
+    #    """Valid return value."""
+    #    parsed = self.parser.parse_args(['--sets', '20'])
+    #    self.assertEqual(parsed.sets, 20)
+
+if __name__ == '__main__':
+    unittest.main()
+
