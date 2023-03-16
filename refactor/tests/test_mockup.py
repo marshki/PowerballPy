dc9a3fa5565058186a12cec53852525e9cbe489d
marshki <marshall@morethantomorrow.org>
2023-03-16 16:25:01 -0400

    the whole enchalada

--- refactor/tests/test_mockup.py
+++ refactor/tests/test_mockup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-"""Testing class for powerball.py setup.
+"""Unit tests for powerbally.py
 """
 
 import random
 import unittest
+from prettytable import PrettyTable
 
 def white_numbers(range_min, range_max, balls):
     """Generate n pseudo-random numbers between min and max range +1.
@@ -64,6 +65,28 @@ class TestRedNumber(unittest.TestCase):
         for red_ball in self.red_ball:
             self.assertGreaterEqual(red_ball, self.range_min)
             self.assertLessEqual(red_ball, self.range_max)
+
+def make_table():
+    """Use 'PrettyTable' module to create table for output. Align left.
+    """
+
+    table = PrettyTable(['Whites #s', 'Red #'])
+    table.align = 'l'
+    return table
+
+class TestMakeTable(unittest.TestCase):
+    """Test class.
+    """
+
+    def test_make_table(self):
+        """Test: table, field names, and alignment.
+        """
+
+        table = make_table()
+        self.assertIsInstance(table, PrettyTable)
+        self.assertEqual(table.field_names, ['Whites #s', 'Red #'])
+        for field in table.field_names:
+            self.assertEqual(table.align[field], 'l')
 
 if __name__ == '__main__':
     unittest.main()
