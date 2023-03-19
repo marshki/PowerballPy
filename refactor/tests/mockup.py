959fd5962bc00863edee2d9c0c7c11741ec9b2ec
marshki <marshall@morethantomorrow.org>
2023-03-19 12:52:42 -0400

    does not work, but adding so as to fix

--- refactor/tests/mockup.py
+++ refactor/tests/mockup.py
@@ -8,6 +8,7 @@ import unittest
 from unittest.mock import patch
 from prettytable import PrettyTable
 from powerballpy import make_table
+from powerballpy import white_numbers
 
 #def make_table():
 #    """Use 'PrettyTable' module to create table for output. Align left.
@@ -30,6 +31,33 @@ class TestMakeTable(unittest.TestCase):
         self.assertEqual(table.field_names, ['Whites #s', 'Red #'])
         for field in table.field_names:
             self.assertEqual(table.align[field], 'l')
+
+class TestWhiteNumbers(unittest.TestCase):
+    """Test class.
+    """
+
+    def setUp(self):
+        self.range_min = 1
+        self.range_max = 69
+        self.balls = 5
+        self.white_balls = white_numbers(self.range_min, self.range_max, self.balls)
+
+    def test_count(self):
+        """Test count of balls genertated.
+        """
+        self.assertEqual(len(self.white_balls), self.balls)
+
+    def test_range(self):
+        """Test white_ball falls within min and max range.
+        """
+        for white_ball in self.white_balls:
+            self.assertGreaterEqual(white_ball, self.range_min)
+            self.assertLessEqual(white_ball, self.range_max)
+
+    def test_duplicates(self):
+        """Test for white_ball duplicates.
+        """
+        self.assertEqual(self.white_balls, set(self.white_balls))
 
 if __name__ == '__main__':
     unittest.main()
