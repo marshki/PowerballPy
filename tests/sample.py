36d9049bb9ceb9eb8ec7721e0fe9b1e535d9f976
marshki <marshall@morethantomorrow.org>
2023-04-06 10:18:31 -0400

    fix pylint errors

--- tests/sample.py
+++ tests/sample.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
-"""Placeholder.
+"""Try/except block for user input. Needs integration with argparse.
+Probably enough to exit for invalid input.
 """
 
 while True:
     number = input("Enter a number: ")
     try:
         value = int(number)
-        if value < 0:
+        if value < 1:
             print("Try again.")
             continue
         break
     except ValueError:
-        print("Not an integer")
+        print("That's not an integer.")
+
 print(value)
