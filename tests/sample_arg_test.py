1a8ea012f2135d2c9277f6d792b2fb1e1fc176d8
marshki <marshall@morethantomorrow.org>
2023-04-26 19:56:03 -0400

    anotha one

--- tests/sample_arg_test.py
+++ tests/sample_arg_test.py
@@ -10,4 +10,8 @@ class TestArgParse(unittest.TestCase):
         args = parser.parse_args([])
         self.assertEqual(args.num_sets, 1)
 
-
+    def test_custom_num_sets(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-n", "--num_sets", type=int, default=1)
+        args = parser.parse_args(['-n', '5'])
+        self.assertEqual(args.num_sets, 5)
