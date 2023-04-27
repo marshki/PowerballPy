7fce4c2488edb885668c2facfb77baa7e0940860
marshki <marshall@morethantomorrow.org>
2023-04-27 13:54:30 -0400

    update

--- tests/sample.py
+++ tests/sample.py
@@ -17,3 +17,17 @@ while True:
         print("That's not an integer.")
 
 print(value)
+
+
+"""
+parser = argparse.ArgumentParser(description="Powerball Number Generator")
+parser.add_argument("-n", "--num_sets", type=int, default=1, help="Number of sets to generate")
+
+args = parser.parse_args()
+
+try:
+    if args.num_sets < 1:
+        raise argparse.ArgumentTypeError("Number of sets must be a positive integer.")
+except argparse.ArgumentTypeError as e:
+    parser.error(str(e))
+""'
