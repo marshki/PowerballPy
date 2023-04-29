26c2a29188081173444926c10c78a0b7f76cb87c
marshki <marshall@morethantomorrow.org>
2023-04-29 16:05:44 -0400

    refactor

--- tests/sample.py
+++ tests/sample.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
 
-"""Try/except block for user input.
-Needs integration with argparse.
-Probably enough to exit for invalid input.
-"""
-
-while True:
-    number = input("Enter a number: ")
-    try:
-        value = int(number)
-        if value < 1:
-            print("Integer must be greater than or equal to one (1).")
-            continue
-        break
-    except ValueError:
-        print("That's not an integer.")
-
-print(value)
-
-
-"""
 parser = argparse.ArgumentParser(description="Powerball Number Generator")
 parser.add_argument("-n", "--num_sets", type=int, default=1, help="Number of sets to generate")
 
@@ -30,4 +10,3 @@ try:
         raise argparse.ArgumentTypeError("Number of sets must be a positive integer.")
 except argparse.ArgumentTypeError as e:
     parser.error(str(e))
-""'
