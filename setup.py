864602785b1424355e132773666d0afa9aabc6fa
marshki <marshall@morethantomorrow.org>
2023-05-25 11:46:52 -0400

    update

--- setup.py
+++ setup.py
@@ -6,10 +6,21 @@ setup(
         author='M. Krinitz',
         author_email='<marshall [at] morethantomorrow [dot] org>',
         description='Pseudo-random number generator for Powerball lottery in Python 3.',
-        url='',
-        packages='',
+        url='https://github.com/marshki/PowerballPy',
+        packages='powerballpy',
         install_requires=[
+            'prettytable',
         ],
         classifiers=[
+
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
         ],
 )
