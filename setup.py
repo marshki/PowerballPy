ebf81a923498da773b1e2e0fb616250bd1111d03
marshki <marshall@morethantomorrow.org>
2023-05-25 11:38:59 -0400

    add skeleton

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+        name='powerballpy',
+        version='0.1.2',
+        author='M. Krinitz',
+        author_email='<marshall [at] morethantomorrow [dot] org>',
+        description='Pseudo-random number generator for Powerball lottery in Python 3.',
+        url='',
+        packages='',
+        install_requires=[
+        ],
+        classifiers=[
+        ],
+)
